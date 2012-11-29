32ebec7c3581c27298150ab26679430a282f3be3
Jonathan Marmor <jm@ex.fm>
2012-11-29 17:12:02 -0500

    Update grunt jenkins task.

--- grunt.js
+++ grunt.js
@@ -19,6 +19,14 @@ module.exports = function(grunt){
         'jshint': {
             'options': {
                 'node': true
+            },
+            'globals': {
+                'it': true,
+                'describe': true,
+                'before': true,
+                'after': true,
+                'beforeEach': true,
+                'afterEach': true
             }
         }
     });
@@ -42,41 +50,94 @@ module.exports = function(grunt){
     });
 
     grunt.registerTask("jenkins", function(){
-        var magneto,
-            env,
+        var env,
             mocha,
             jshint,
-            done = this.async();
+            done = this.async(),
+            packageJson,
+            testCode = -1;
 
-        magneto = grunt.utils.spawn({
-            'cmd': "magneto",
-            'args': ["8888"]
-        });
         env = grunt.utils._.extend(process.env, {
             'JENKINS': 1,
-            'MAGNETO_PORT': 8888
+            'NODE_ENVIRONMENT': "testing"
         });
 
-        mocha = grunt.utils.spawn({
-            'cmd': "mocha",
-            'args': ["-R", "xunit"],
-            'opts': {
-                'env': env
-            }
-        }, function(error, result, code){
-            fs.writeFileSync("xunit.xml", result.stdout);
-            jshint = grunt.utils.spawn({
-                'cmd': "jshint",
-                'args': [
-                    "./lib",
-                    "./test",
-                    "--jslint-reporter"
-                ]
-            }, function(error, result, code){
-                fs.writeFileSync("jshint.xml", result.stdout);
-                magneto.kill();
-                done();
+        function rmNodeModules(dependencies){
+            var p, d = when.defer();
+
+            when.all(Object.keys(dependencies).map(function(key){
+                if(dependencies[key].indexOf("github.com") !== -1){
+                    p = when.defer();
+                    child_process.exec("rm -rf node_modules/" + key, function(err, stdout, stderr) {
+                        if(err){
+                            grunt.warn(err);
+                        }
+                        p.resolve();
+                    });
+                }
+            }), d.resolve);
+            return d.promise;
+        }
+
+        sequence(this).then(function(next){
+            // Read package.json
+            packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+
+            // Delete dependencies with github source
+            rmNodeModules(packageJson.dependencies).then(next);
+        }).then(function(next){
+            // Delete devDependencies with github source
+            rmNodeModules(packageJson.devDependencies).then(next);
+        }).then(function(next){
+            // Run npm install
+            child_process.exec("npm install", function(err, stdout, stderr) {
+                if(err){
+                    grunt.warn(err);
+                }
+                next();
             });
+        }).then(function(next){
+            child_process.exec("rm -f xunit.xml jshint.xml", function(err, stdout, stderr) {
+                if(err){
+                    grunt.warn(err);
+                }
+                next();
+            });
+        }).then(function(next){
+            mocha = grunt.utils.spawn(
+                {
+                    'cmd': "mocha",
+                    'args': ["-R", "xunit"],
+                    'opts': {
+                        'env': env
+                    }
+                },
+                next
+            );
+        }).then(function(next, error, result, code){
+            console.log('Tests passed? ', (code === 0) ? 'yes' : 'no');
+            console.log(result.stdout);
+            console.error(result.stderr);
+            testCode = code;
+
+            // Write test results
+            fs.writeFileSync("xunit.xml", result.stdout);
+
+            // Run jshint
+            jshint = grunt.utils.spawn(
+                {
+                    'cmd': "jshint",
+                    'args': [
+                        "./lib",
+                        "./test",
+                        "--jslint-reporter"
+                    ]
+                },
+                next
+            );
+        }).then(function(next, error, result, code){
+            fs.writeFileSync("jshint.xml", result.stdout);
+            process.exit(testCode);
         });
     });
 
